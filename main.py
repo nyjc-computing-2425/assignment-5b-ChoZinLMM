2f3750b73506db115db57fcf9172dd8e7a42e663
ChoZinLMM <160577451+ChoZinLMM@users.noreply.github.com>
2024-03-25 10:39:51 +0000

    1st commit

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 # Part 1
-def read_csv(filename):
-    # Type your code below
-    pass
+# def read_csv(filename):
+    # filename = 'pre-u-enrolment-by-age.csv'
+with open('pre-u-enrolment-by-age.csv', 'r') as f:
+  # header = list(f.readline().strip().split(','))
+  # print(header)
 
 
-# Part 2
-def filter_gender(enrolment_by_age, sex):
-    # Type your code below
-    pass
+
+  data = []
+  for line in f:
+    record = list(line.strip().split(','))
+    record[0] = int(record[0])
+    record[]
+    
+    data.append(record)
+  print(data)
+        
+    # pass
 
 
-# Part 3
-def sum_by_year(enrolment):
-    # Type your code below
-    pass
+# # Part 2
+# def filter_gender(enrolment_by_age, sex):
+#     # Type your code below
+#     pass
 
 
-# Part 4
-def write_csv(filename, header, data):
-    # Type your code below
-    pass
+# # Part 3
+# def sum_by_year(enrolment):
+#     # Type your code below
+#     pass
 
 
-# TESTING
-# You can write code below to call the above functions
-# and test the output
+# # Part 4
+# def write_csv(filename, header, data):
+#     # Type your code below
+#     pass
+
+
+# # TESTING
+# # You can write code below to call the above functions
+# # and test the output
