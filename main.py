e058955ad1d59bb0c929a777c175139009e4167f
ChoZinLMM <160577451+ChoZinLMM@users.noreply.github.com>
2024-03-27 06:53:21 +0000

    Draft commit

--- main.py
+++ main.py
@@ -47,41 +47,25 @@
 #         list 1 - data of male enrolment without sex
 #         list 2 - data of female enrolment without sex
 # #     """
-sex_m = "MF"
-sex_f = "F"
-with open('pre-u-enrolment-by-age.csv', 'r') as f:
-  header = list(f.readline().strip().split(','))
-  data = []
-  sex_mdata = []
-  sex_fdata = []
-  for line in f:
-    record = line.strip().split(',')
-    record[0] = int(record[0])
-    record[3] = int(record[3])
-    data.append(record)
-
-    if sex_m in record[2]:
-      sex_mdata.append(record)
-      for row in (0, 4):
-        sex_mdata.remove(sex_mdata[2])
-    print(sex_mdata) 
-          
-  #   elif sex_f in record[2]:
-  #     sex_fdata.append(record)
-  #     sex_fdata.remove("F")
-  # print(sex_fdata)
-
-        
-
+#     sex_m = "MF"
+#     sex_f = "F"
     
-          
-    # pass
+#     MF_sexrecord = []
+#     F_sexrecord = []
+#     for row in enrolment_by_age:
+#       if sex_m in row[2]:
+#         MF_sexrecord.append(row[0], row[1], row[3])
+#         return MF_sexrecord
+    
+#       elif sex_f in row[2]:
+#         F_sexrecord.append(row[0], row[1], row[3])
+#         return F_sexrecord
 
 
 # Part 3
-def sum_by_year(enrolment_data):
-    # Type your code below
-    pass
+# def sum_by_year(enrolment_data):
+#     # Type your code below
+#     pass
 
 
 # # Part 4
@@ -93,3 +77,4 @@ def sum_by_year(enrolment_data):
 # # TESTING
 # # You can write code below to call the above functions
 # # and test the output
+
